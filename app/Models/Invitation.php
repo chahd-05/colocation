0eeddf2f3df52535b46ef847f052fbe6b47c0a43
shahd <mazouzchahd12@gmail.com>
2026-02-25 09:36:40 +0000

    feat: add fillable attributes to Invitation model

--- app/Models/Invitation.php
+++ app/Models/Invitation.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Invitation extends Model
 {
-    //
+    protected $fillable = [
+        'email',
+        'colocation_id',
+        'token',
+        'expires_at',
+        'accepted_at',
+        'refused_at'
+    ];
 }
