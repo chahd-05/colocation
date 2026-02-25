f81298038eb15ba11bcdf2c833bdb2f635a28f7d
shahd <mazouzchahd12@gmail.com>
2026-02-25 09:36:19 +0000

    feat: add fillable attributes to Expense model

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Expense extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'amount',
+        'expenses_date',
+        'colocation_id',
+        'payer_id',
+        'category_id'
+    ];
 }
