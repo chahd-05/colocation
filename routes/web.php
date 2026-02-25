45460303c6d5f697af6196917fbe4e966b12524f
shahd <mazouzchahd12@gmail.com>
2026-02-25 11:16:52 +0000

    feat: fix ColocationController route definitions in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ColocationController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/colocations/create', [ColocationController::class, 'create'])->name('colocoations.create');
+    Route::post('/colocations', [ColocationController::class], 'store')->name('colocations.store');
+    Route::get('/colocations/{colocation}', [ColocationController::class], 'show')->name('colocations.show');
 });
 
 require __DIR__.'/auth.php';
