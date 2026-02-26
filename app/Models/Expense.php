71187b6c74ff4b4cfc3c5d0bfe8072f02b3cb780
shahd <mazouzchahd12@gmail.com>
2026-02-26 10:50:53 +0000

    feat: add casts and relationships for Expense model

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 
 class Expense extends Model
@@ -14,4 +15,26 @@ class Expense extends Model
         'payer_id',
         'category_id'
     ];
+
+    protected function casts(): array
+    {
+        return [
+            'expenses_date' => 'datetime',
+        ];
+    }
+
+    public function colocation(): BelongsTo
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function payer(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'payer_id');
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
